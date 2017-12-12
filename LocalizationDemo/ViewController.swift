f382070c4da960cce6811e6362f1c60328a03b8d
lokizero00 <lokizero0@live.com>
2017-12-12 15:49:03 +0800

    Initial Commit

---
+++ LocalizationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by lokizero00 on 2017/12/12.
+//  Copyright © 2017年 lokizero00. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
